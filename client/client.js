4cd3ba73e8e9a286202d8076010ffb4421ecb5f9
Logan Willis <loganw0315@gmail.com>
2021-11-24 19:49:10 -0600

    first commit

---
+++ client/client.js
@@ -0,0 +1,206 @@
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const resultsButton = document.getElementById('results-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const correctSound = new Audio('correct.wav')
+const wrongSound = new Audio('wrong.mp3')
+const resultsList = document.getElementById('question-results-list')
+// const resultsHeader = document.getElementById('results-header')
+const selectQuizButton = document.getElementById('dropbtn')
+const menuButton = document.getElementById('menu-btn')
+
+let shuffledQuestions, currentQuestionIndex, questions
+
+document.getElementById('quiz-1').addEventListener("click", () => {
+  selectQuizButton.textContent="Quiz 1"
+  axios.put(`/api/quiz/quiz1`)
+    .then(res =>{
+      console.log(res.data);
+    })
+})
+
+
+menuButton.addEventListener('click', goToMainMenu)
+startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+resultsButton.addEventListener('click', showResults)
+
+function startQuiz() {
+  if(selectQuizButton.textContent !== "Select Quiz"){
+  startButton.classList.add('hide')
+  selectQuizButton.classList.add('hide')
+  menuButton.classList.remove('hide')
+  axios.get(`/api/quiz`)
+    .then(res =>{
+      questions = res.data
+      console.log(res.data);
+      shuffledQuestions = questions.sort(() => Math.random() - .5)
+      currentQuestionIndex = 0
+      questionContainerElement.classList.remove('hide')
+      setNextQuestion()
+    })
+  }else{
+    alert("Please select a quiz");
+  }
+}
+
+function goToMainMenu() {
+  // const resultsTotalElement = document.querySelectorAll('p')
+  // if(resultsTotalElement){
+  //   resultsTotalElement.forEach(elem => resultsHeader.removeChild(resultsTotalElement)
+  // }
+  resultsHeader.innerHTML=""
+  resetPage()
+  menuButton.classList.add('hide')
+  selectQuizButton.classList.remove('hide')
+  startButton.classList.remove('hide')
+  resultsHeader.classList.add('hide')
+  resultsButton.classList.add('hide')
+  questionContainerElement.classList.add('hide')
+  
+  selectQuizButton.textContent = "Select Quiz"
+  startButton.textContent = "Begin Quiz"
+  currentQuestionIndex = 0
+  resultsList.innerHTML="" 
+  Array.from(answerButtonsElement.children).forEach(button => {
+    button.classList.add('hide')
+  })
+}
+
+function setNextQuestion() {
+  resetPage()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionContainerElement.classList.remove('hide')
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetPage() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  questionElement.classList.remove('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+  Array.from(answerButtonsElement.children).forEach(button => {
+    button.classList.remove('hide')
+  })
+
+  resultsList.classList.add('hide')
+  // resultsHeader.classList.add('hide')
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  let answer = {
+      question: questionElement.textContent,
+      correct: correct
+  }
+  axios.post(`/api/stats/`, answer)
+    .then(res => {
+      console.log(res);
+    })
+  if(correct){
+    correctSound.play()
+    party.confetti(this, {
+        count: party.variation.range(20, 40),
+    });
+    correctSound.play()
+  }else{
+    wrongSound.play()
+  }
+  
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+    button.removeEventListener('click', selectAnswer)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    resultsButton.classList.remove('hide')
+  }
+}
+function showResults(){
+    questionElement.classList.add('hide')
+    resultsList.classList.remove('hide')
+    // resultsHeader.classList.remove('hide')
+    resultsButton.classList.add('hide')
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+    let resultsHeader = document.createElement('div')
+    resultsHeader.id = 'results-header'
+    resultsHeader.textContent = 'Results'
+    questionContainerElement.prepend(resultsHeader)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        button.classList.add('hide')
+      })
+    resultsList.innerHTML=""  
+    
+    axios.get(`/api/stats/`)
+    .then(res => {
+        
+    for(let i=0; i<res.data[0].length;i++){
+    let {question, correct} = res.data[0][i]    
+    let questionResult = document.createElement('li')
+    let questionResultQuestion = document.createElement('span')
+    let questionResultPicture = document.createElement('img')
+    questionResultQuestion.textContent= `${question} ` 
+    questionResultPicture.classList.add('result-picture')
+    if(correct){
+      questionResultPicture.src="/images/greencheck.png"
+    }else{
+      questionResultPicture.src="/images/redx.png"
+    }
+    questionResult.appendChild(questionResultQuestion)
+    questionResult.appendChild(questionResultPicture)
+    resultsList.appendChild(questionResult)  
+    }
+
+    let correctCount = res.data[1]
+    let wrongCount = res.data[2]
+    let totalQuestions = (+correctCount)+(+wrongCount)
+    let resultsTotal = document.createElement('p')
+    resultsTotal.textContent = `${correctCount}/${totalQuestions}`
+    resultsHeader.appendChild(resultsTotal)
+    axios.delete(`/api/stats/`)
+    totalQuestions = 0;
+    })
+
+
+    
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
---
+++ server/server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const path = require('path')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('client'))
+app.use(express.static(path.join(__dirname, 'client/client.js')))
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const quiz1 = require("./quiz-1.json")
+// const quiz2 = require("./quiz-2.json")
+// const quiz3 = require("./quiz-3.json")
+
+let currentquiz
+
+let stats = []
+let answerId = 0;
+let correctCount = 0;
+let wrongCount = 0;
+
+app.get(`/api/stats`, (req,res) => {
+    res.status(200).send([stats,correctCount,wrongCount])
+})
+
+app.post(`/api/stats`, (req, res) => {
+  let {question, correct} = req.body
+  let answer = {
+    id: answerId,
+    question,
+    correct
+  }
+  stats.push(answer)
+  res.status(200).send(answer)
+  if(correct){
+    correctCount++
+  }else{
+    wrongCount++
+  }
+  answerId++
+})
+
+app.delete(`/api/stats`, (req,res) => {
+  stats=[]
+  answerId=0
+  correctCount=0
+  wrongCount=0
+})
+
+app.get(`/api/quiz`, (req,res) => {
+  res.status(200).send(currentquiz)
+})
+
+app.put(`/api/quiz/:quiz`, (req,res) => {
+  
+  if(req.params.quiz == "quiz1"){
+    currentquiz = quiz1
+  // }else if(req.params == "quiz2"){
+  //   currentquiz = quiz2
+  // }else if(req.params == "quiz3"){
+  //   currentquiz = quiz3
+  }
+  res.status(200).send(currentquiz)
+})
+
+
+
+
+
+const port = process.env.PORT || 4000
+app.listen(port, ()=>{
+    console.log(`App is running on ${port}`);
+})
